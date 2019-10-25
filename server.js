8825e5428f056a2a9bc8a48409b0c43e7ed20a64
shaoyih <shaoyih2@gmail.com>
2019-10-25 08:28:49 -0700

    set all of mongoDB setups and created routes files

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+connectDB();
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
