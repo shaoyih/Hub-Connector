142d2dcabc5a7de78878e6b501e99363e864e1c0
shaoyih <shaoyih2@gmail.com>
2019-10-25 08:47:40 -0700

    test routes

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ const app = express();
 
 connectDB();
 app.get('/', (req, res) => res.send('API Running'));
+
+//Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
