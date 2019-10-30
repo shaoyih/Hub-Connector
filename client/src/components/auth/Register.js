a0038037aa1c6f6db38132860df9a522810f94fa
Shaoyi <shaoyih2@gmail.com>
2019-10-29 18:35:07 -0700

    added features of using register API from backend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,11 +2,12 @@ import React, {Fragment, useState} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {setAlert } from '../../actions/alert';
+import {register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -18,24 +19,26 @@ const Register = ({setAlert}) => {
 
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
             setAlert('Password do not match','danger');
         }else{
-            console.log('Success');
+            register({name,email,password});
+            
         }
-    }
+    };
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
             <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+                <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)}  />
                 </div>
                 <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required/>
+                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
                 <small className="form-text">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                 </div>
                 <div className="form-group">
@@ -43,7 +46,7 @@ const Register = ({setAlert}) => {
                     type="password"
                     placeholder="Password"
                     name="password"
-                    minLength="6"
+                    
                     value={password} 
                     onChange={e => onChange(e)}
                 />
@@ -53,7 +56,7 @@ const Register = ({setAlert}) => {
                     type="password"
                     placeholder="Confirm Password"
                     name="password2"
-                    minLength="6"
+                   
                     value={password2}
                     onChange={e => onChange(e)}
                 />
@@ -68,7 +71,8 @@ const Register = ({setAlert}) => {
 };
 
 Register.prototypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
 }
 // first one is state, second one is alert
-export default connect(null ,{setAlert})(Register);
+export default connect(null ,{setAlert, register})(Register);
