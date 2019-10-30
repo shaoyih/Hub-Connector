e77392f3569355f59e5d5c572124acc230909132
Shaoyi <shaoyih2@gmail.com>
2019-10-30 11:19:53 -0700

    initialized dashboard reducers and different options, added spinner

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {setAlert} from './alert';
-import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED,AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL,LOGOUT} from './types';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED,AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL,LOGOUT, CLEAR_PROFILE} from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 
@@ -90,5 +90,7 @@ export const login = (email, password) => async dispatch =>{
 
 //LOG OUT / CLEAR Profile
 export const logout = () => dispatch =>{
+    dispatch({type: CLEAR_PROFILE});
     dispatch({type: LOGOUT});
+    
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,6 +7,11 @@ import PropTypes from 'prop-types';
 const Navbar = ({auth: {isAuthenticated, loading} , logout}) => {
   const authLinks = (
     <ul>
+      <li><Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
       <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt' />{' '}
@@ -25,7 +30,7 @@ const Navbar = ({auth: {isAuthenticated, loading} , logout}) => {
     return (
     <nav className="navbar bg-dark">
         <h1>
-          <Link to="index.html"><i class="fas fa-code"></i> DevConnector</Link>
+          <Link to="/"><i class="fas fa-code"></i> DevConnector</Link>
         </h1>
       
         {!loading &&  (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
