62bc0475835762e35456ade879cdf016537bd70e
Shaoyi <shaoyih2@gmail.com>
2019-10-31 14:24:50 -0700

    Implmented add or delete post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {setAlert} from './alert';
-import{GET_POSTS, POST_ERROR, UPDATE_LIKES} from './types'
+import{GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST} from './types'
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -47,6 +47,51 @@ export const removeLike = postId => async dispatch => {
       type: UPDATE_LIKES,
       payload: {postId, likes: res.data}
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+
+// Delete post
+export const deletePost = postId => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/${postId}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: postId
+    });
+
+    dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+
+// Add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  try {
+    const res = await axios.post('/api/posts',formData,config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
