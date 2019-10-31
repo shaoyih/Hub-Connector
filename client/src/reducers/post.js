90719beb5cc95a3c9fcfff33878f14ace71fe3cf
Shaoyi <shaoyih2@gmail.com>
2019-10-31 16:40:09 -0700

    add ALL

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -55,6 +57,18 @@ export default function(state = initialState, action){
                     {...post, likes: payload.likes} : post),
                 loading: false
             }
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: {...state.post, comments: payload},
+                lodaing: false
+            }
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {...state.post, comments: state.post.comments.filter(comment => comment.id !== payload)},
+                lodaing: false
+            }    
         default:
             return state;
     }
