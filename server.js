48779d1fe55a05be7fd6d48fd257df480a16f327
Shaoyi <shaoyih2@gmail.com>
2019-11-02 11:23:06 -0700

    setup again

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
-const PORT = process.env.PORT || 6000;
+const PORT =  process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
